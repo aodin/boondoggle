11ad0f0306d4cb72db0c1d2952406ae63470df87
aodin <aaronoellis@gmail.com>
2014-03-27 13:19:57 -0600

    Added Referer to request logger

--- logger.go
+++ logger.go
@@ -20,10 +20,11 @@ func (l *DefaultLogger) Log(r *http.Request) {
 		ip = strings.SplitN(r.RemoteAddr, ":", 2)[0]
 	}
 	log.Printf(
-		`%q %s %q`,
+		`%q %s %q %q`,
 		fmt.Sprintf(`%s %s`, r.Method, r.URL),
 		ip,
 		r.Header.Get("User-Agent"),
+		r.Header.Get("Referer"),
 	)
 }
 
